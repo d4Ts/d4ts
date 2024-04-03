0d17be1e08421757f5e2003f32cb44ec1cd7fe4d
d4ntes <102186985+d4Ts@users.noreply.github.com>
2024-04-03 22:27:09 +0300

    Add files via upload

--- paper/news/views.py
+++ paper/news/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from datetime import datetime
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class PostList(ListView):
+    model = Post
+    ordering = 'datetime_of_appear'
+    template_name = 'default.html'
+    context_object_name = 'post'
+
+    # Метод get_context_data позволяет нам изменить набор данных,
+    # который будет передан в шаблон.
+    def get_context_data(self, **kwargs):
+        # С помощью super() мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответе мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        context['next_sale'] = None
+        return context
+
+class PostDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Post
+    # Используем другой шаблон — product.html
+    template_name = 'default.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'post'
